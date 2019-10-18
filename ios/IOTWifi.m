bdfba56541e7916329ae40fd5f661eb1c99b5f61
Zach Moore <get3moore@gmail.com>
2019-10-18 18:41:12 -0400

    iOS: added logic to trigger error if connection fails
    #26

--- ios/IOTWifi.m
+++ ios/IOTWifi.m
@@ -12,54 +12,81 @@
         }
         callback(@[available]);
     }
-    
+
     RCT_EXPORT_METHOD(connect:(NSString*)ssid
                       bindNetwork:(BOOL)bindNetwork //Ignored
                       callback:(RCTResponseSenderBlock)callback) {
         if (@available(iOS 11.0, *)) {
             NEHotspotConfiguration* configuration = [[NEHotspotConfiguration alloc] initWithSSID:ssid];
             configuration.joinOnce = !bindNetwork;
-            
+
             [[NEHotspotConfigurationManager sharedManager] applyConfiguration:configuration completionHandler:^(NSError * _Nullable error) {
                 if (error != nil) {
                     callback(@[@"Error while configuring WiFi"]);
                 } else {
-                    callback(@[[NSNull null]]);
+
+                    NSString *kSSID = (NSString*) kCNNetworkInfoKeySSID;
+
+                    NSArray *ifs = (__bridge_transfer id)CNCopySupportedInterfaces();
+                    for (NSString *ifnam in ifs) {
+                      NSDictionary *info = (__bridge_transfer id)CNCopyCurrentNetworkInfo((__bridge CFStringRef)ifnam);
+                      if (info[kSSID] == ssid) {
+                        callback(@[[NSNull null]]);
+                        return;
+                      } else {
+                          callback(@[[NSString  stringWithFormat:@"Unable to join %@", ssid]]);
+                        return;
+                      }
+                    }
+
                 }
             }];
-            
+
         } else {
             callback(@[@"Not supported in iOS<11.0"]);
         }
     }
-        
+
     RCT_EXPORT_METHOD(connectSecure:(NSString*)ssid
                       withPassphrase:(NSString*)passphrase
                       isWEP:(BOOL)isWEP
                       bindNetwork:(BOOL)bindNetwork
                       callback:(RCTResponseSenderBlock)callback) {
-        
+
         if (@available(iOS 11.0, *)) {
             NEHotspotConfiguration* configuration = [[NEHotspotConfiguration alloc] initWithSSID:ssid passphrase:passphrase isWEP:isWEP];
             configuration.joinOnce = !bindNetwork;
-            
+
             [[NEHotspotConfigurationManager sharedManager] applyConfiguration:configuration completionHandler:^(NSError * _Nullable error) {
                 if (error != nil) {
                     callback(@[[error localizedDescription]]);
                 } else {
-                    callback(@[[NSNull null]]);
+
+                    NSString *kSSID = (NSString*) kCNNetworkInfoKeySSID;
+
+                    NSArray *ifs = (__bridge_transfer id)CNCopySupportedInterfaces();
+                    for (NSString *ifnam in ifs) {
+                      NSDictionary *info = (__bridge_transfer id)CNCopyCurrentNetworkInfo((__bridge CFStringRef)ifnam);
+                      if (info[kSSID] == ssid) {
+                        callback(@[[NSNull null]]);
+                        return;
+                      } else {
+                          callback(@[[NSString  stringWithFormat:@"Unable to join %@", ssid]]);
+                        return;
+                      }
+                    }
                 }
             }];
-            
+
         } else {
             callback(@[@"Not supported in iOS<11.0"]);
         }
     }
-    
+
     RCT_EXPORT_METHOD(removeSSID:(NSString*)ssid
                       unbindNetwork:(BOOL)unbindNetwork //Ignored
                       callback:(RCTResponseSenderBlock)callback) {
-        
+
         if (@available(iOS 11.0, *)) {
             [[NEHotspotConfigurationManager sharedManager] getConfiguredSSIDsWithCompletionHandler:^(NSArray<NSString *> *ssids) {
                 if (ssids != nil && [ssids indexOfObject:ssid] != NSNotFound) {
@@ -70,9 +97,9 @@
         } else {
             callback(@[@"Not supported in iOS<11.0"]);
         }
-        
+
     }
-    
+
     RCT_REMAP_METHOD(getSSID,
                      callback:(RCTResponseSenderBlock)callback) {
         NSString *kSSID = (NSString*) kCNNetworkInfoKeySSID;
@@ -90,4 +117,3 @@
         callback(@[@"Cannot detect SSID"]);
     }
 @end
-
